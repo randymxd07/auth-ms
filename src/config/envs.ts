261afa5c2f9663909bb20f7ee95216d3dadccbcc
randymxd06 <randym0624@gmail.com>
2024-06-13 19:25:50 -0400

    feat: :sparkles: Configure the project environment variables and they are being validated

---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+    PORT: number;
+    // DATABASE_URL: string;
+}
+
+const envsSchema = joi.object({
+    PORT: joi.number().required(),
+    // DATABASE_URL: joi.string().required(),
+})
+.unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+    port: envVars.PORT,
+    // databaseUrl: envVars.DATABASE_URL,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+
+  const logger = new Logger('Main Auth Microservice');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(envs.port);
+
+  logger.log(`Auth Microservice running on port: ${envs.port}`);
+
 }
 bootstrap();
